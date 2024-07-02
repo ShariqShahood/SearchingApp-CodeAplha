4312830b6dcf91e03e2cfb6e66bf5be0eef7c029
SHARIQ SHAHOOD <137090341+ShariqShahood@users.noreply.github.com>
2024-07-02 16:41:20 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,305 @@
+var phones = {
+    Motrolla: {
+        Ultra_Max: {
+            image: "https://cdn0.vox-cdn.com/hermano/verge/product/image/7222/43.JPG",
+            Ram: '2GB',
+            Rom: '16GB',
+            Os: 'Version 2.0',
+            price: 'Rs 8000',
+            camera: '13 mega-pixel',
+            color: ['red', 'black', 'silver']
+        },
+        Ultra_Max_Turbo: {
+            image: "https://ghost-armor.com/media/catalog/product/cache/b364ca7b96ca47e9732299f5b78474d1/m/o/moto_droid_maxx_2.png",
+            Ram: '3GB',
+            Rom: '32GB',
+            Os: 'Version 7.0',
+            price: 'Rs 18000',
+            camera: '18 mega-pixel',
+            color: ['red', 'black', 'silver']
+        },
+        edge5G: {
+            image: "https://www.notebookcheck.net/uploads/tx_nbc2/4_zu_3_Motorola_Edge.jpg",
+            Ram: '4GB',
+            Rom: '64GB',
+            Os: 'Version 9.0',
+            price: 'Rs 28000',
+            camera: '35 mega-pixel',
+            color: ['grey', 'black', 'maroon']
+        },
+        MotoX: {
+            image: "https://mpc.com.pk/adminnew/upload/1743180-motorola-moto-x-play-(2016)-3d.png",
+            Ram: '8GB',
+            Rom: '128GB',
+            Os: 'Version 11',
+            price: 'Rs 58000',
+            camera: '60 mega-pixel',
+            color: ['grey', 'black', 'white']
+        }
+    },
+    Redmi: {
+        Note_10: {
+            image: "https://i0.wp.com/mobilemall.com.pk/wp-content/uploads/2022/07/Xiaomi-Redmi-Note-10-Lite-price-in-Pakistan.jpg?fit=511%2C599&ssl=1",
+            Ram: '6GB',
+            Rom: '32GB',
+            Os: 'Version 9.0',
+            price: 'Rs 48000',
+            camera: '48 mega-pixel',
+            color: ['gold', 'black', 'maroon']
+        },
+        Note_11: {
+            image: "https://www.pakmobizone.pk/wp-content/uploads/2022/01/Xiaomi-Redmi-Note-11-Twilight-Blue-1.jpg",
+            Ram: '8GB',
+            Rom: '128GB',
+            Os: 'Version 10',
+            price: 'Rs 80000',
+            camera: '58 mega-pixel',
+            color: ['gold', 'black', 'maroon']
+        },
+        Note_12: {
+            image: "https://a2zmobileprices.com/assets/images/models/featured/8c348c2b9657cdbe71153a711852ac1d.jpg",
+            Ram: '16GB',
+            Rom: '256GB',
+            Os: 'Version 13',
+            price: 'Rs 128000',
+            camera: '60 mega-pixel',
+            color: ['gold', 'black', 'maroon']
+        },
+        Redmi_10C: {
+            image: "https://www.i4u.com.pk/upload_new/Xiaomi-Redmi-10C-4GB-128GB.webp",
+            Ram: '18GB',
+            Rom: '128GB',
+            Os: 'Version 12',
+            price: 'Rs 98000',
+            camera: '58 mega-pixel',
+            color: ['grey', 'black', 'white']
+        }
+
+    },
+    Samsung: {
+        Galaxy_A54: {
+            image: "https://shop.samsung.com/latin/pub/media/catalog/product/cache/a69170b4a4f0666a52473c2224ba9220/s/m/sm-a546_galaxy-a54-5g-all-name_3.png",
+            Ram: '6GB',
+            Rom: '32GB',
+            Os: 'Version 9.0',
+            price: 'Rs 48000',
+            camera: '48 mega-pixel',
+            color: ['gold', 'black', 'maroon']
+        },
+        Galaxy_S23Ultra: {
+            image: "https://images.samsung.com/ae/smartphones/galaxy-s23-ultra/buy/kv_online_exclusive_MO.jpg",
+            Ram: '8GB',
+            Rom: '128GB',
+            Os: 'Version 10',
+            price: 'Rs 80000',
+            camera: '58 mega-pixel',
+            color: ['gold', 'black', 'maroon']
+        },
+        Galaxy_A14: {
+            image: "https://alfamall.com/pub/media/catalog/product/cache/1a89dbb01e019c7ecb20ce1cadff1320/s/a/samsung_a14_-_a_3_1.jpg",
+            Ram: '16GB',
+            Rom: '256GB',
+            Os: 'Version 13',
+            price: 'Rs 128000',
+            camera: '60 mega-pixel',
+            color: ['gold', 'black', 'maroon']
+        },
+        Galaxy_A13: {
+            image: "https://images.samsung.com/is/image/samsung/p6pim/pk/feature/164028106/pk-feature-minimalist-design-with-striking-style-532103593?$FB_TYPE_B_JPG$",
+            Ram: '18GB',
+            Rom: '128GB',
+            Os: 'Version 12',
+            price: 'Rs 98000',
+            camera: '58 mega-pixel',
+            color: ['grey', 'black', 'white']
+        }
+    },
+    Vivo: {
+        Y_21: {
+            image: "https://www.pakmobizone.pk/wp-content/uploads/2021/11/Vivo-Y21-Diamond-Glow-6.jpg",
+            Ram: '6GB',
+            Rom: '32GB',
+            Os: 'Version 9.0',
+            price: 'Rs 48000',
+            camera: '48 mega-pixel',
+            color: ['gold', 'black', 'maroon']
+        },
+        V_27: {
+            image: "https://i.gadgets360cdn.com/large/vivo_v27_pro_1677656640849.jpg",
+            Ram: '8GB',
+            Rom: '128GB',
+            Os: 'Version 10',
+            price: 'Rs 80000',
+            camera: '58 mega-pixel',
+            color: ['gold', 'black', 'maroon']
+        },
+        Vivo_X100: {
+            image: "https://cdn.wccftech.com/wp-content/uploads/2023/11/Vivo-X100-Pro-1.jpg",
+            Ram: '16GB',
+            Rom: '256GB',
+            Os: 'Version 13',
+            price: 'Rs 128000',
+            camera: '60 mega-pixel',
+            color: ['red', 'black', 'maroon']
+        },
+        Y_17S: {
+            image: "https://fdn2.gsmarena.com/vv/pics/vivo/vivo-y55-5g-2.jpg",
+            Ram: '18GB',
+            Rom: '128GB',
+            Os: 'Version 12',
+            price: 'Rs 98000',
+            camera: '58 mega-pixel',
+            color: ['grey', 'black', 'white']
+        }
+    },
+    Oppo: {
+        A_95: {
+            image: "https://propakistani.pk/price/wp-content/uploads/2022/02/Oppo-A95-5G-price-1.png",
+            Ram: '6GB',
+            Rom: '32GB',
+            Os: 'Version 9.0',
+            price: 'Rs 48000',
+            camera: '48 mega-pixel',
+            color: ['gold', 'black', 'maroon']
+        },
+        Reno_10Pro: {
+            image: "https://a2zmobileprices.com/assets/images/models/featured/464789bf776ff9143e2ef6d8e994eb79.jpg",
+            Ram: '8GB',
+            Rom: '128GB',
+            Os: 'Version 10',
+            price: 'Rs 80000',
+            camera: '58 mega-pixel',
+            color: ['gold', 'black', 'maroon']
+        },
+        A_78: {
+            image: "https://image.oppo.com/content/dam/oppo/product-asset-library/specs/a78/a78-bpb.png",
+            Ram: '16GB',
+            Rom: '256GB',
+            Os: 'Version 13',
+            price: 'Rs 128000',
+            camera: '60 mega-pixel',
+            color: ['gold', 'black', 'maroon']
+        },
+        Reno_8_5G: {
+            image: "https://image.oppo.com/content/dam/oppo/product-asset-library/specs/reno8/Reno8-specs.png",
+            Ram: '18GB',
+            Rom: '128GB',
+            Os: 'Version 12',
+            price: 'Rs 98000',
+            camera: '58 mega-pixel',
+            color: ['grey', 'black', 'white']
+        }
+    },
+    Iphone: {
+        iphone_15: {
+            image: "https://images.macrumors.com/t/nn3AJU8joMqZXHxeFf1pojB-klc=/1200x1200/smart/article-new/2023/08/iPhone-15-All-Colors-Mock-Feature.jpg",
+            Ram: '6GB',
+            Rom: '32GB',
+            Os: 'Version 9.0',
+            price: 'Rs 48000',
+            camera: '48 mega-pixel',
+            color: ['gold', 'black', 'maroon']
+        },
+        iphone_14: {
+            image: "https://images.priceoye.pk/apple-iphone-14-pro-pakistan-priceoye-1qdxc.jpg",
+            Ram: '8GB',
+            Rom: '128GB',
+            Os: 'Version 10',
+            price: 'Rs 80000',
+            camera: '58 mega-pixel',
+            color: ['gold', 'black', 'maroon']
+        },
+        iphone_12: {
+            image: "https://www.apple.com/newsroom/images/product/iphone/geo/apple_iphone-12-spring21_hero_geo_04202021_big.jpg.large.jpg",
+            Ram: '16GB',
+            Rom: '256GB',
+            Os: 'Version 13',
+            price: 'Rs 128000',
+            camera: '60 mega-pixel',
+            color: ['gold', 'black', 'maroon']
+        },
+        iphone_X: {
+            image: "https://ng.jumia.is/unsafe/fit-in/500x500/filters:fill(white)/product/37/3652852/1.jpg?9083",
+            Ram: '18GB',
+            Rom: '128GB',
+            Os: 'Version 12',
+            price: 'Rs 98000',
+            camera: '58 mega-pixel',
+            color: ['grey', 'black', 'white']
+        }
+    }
+}
+var main = document.getElementById('main');
+var brand = document.getElementById('brand');
+var model = document.getElementById('model');
+brand.innerHTML += `<option>Select Brand </option>`;
+model.innerHTML += `<option>Select Model </option>`;
+
+function rendorAllPhones() {
+            for (var key in phones) {
+        brand.innerHTML += `<option value="${key}">${key}</option>`;
+        for (var key1 in phones[key]) {
+            var obj = phones[key][key1];
+            console.log(obj.color);
+            var colorDiv = "";
+            for (var i = 0; i < obj.color.length; i++) {
+                colorDiv += `<div class='${obj.color[i]}'></div>`
+                console.log()
+            }
+            main.innerHTML += `
+        <div class="col-12 col-sm-12 col-md-6 col-lg-3 mb-5">
+        <div class="card" style="width: 100%;">
+            <img class="logo" src="${obj.image || 'https://lh3.googleusercontent.com/proxy/g6DJshPxEjru2pMIloPNQgcTFZIlS1x50nSORIFWBJZMsJA0OHlCtwjC3GE0dCyBlqVpZ5YDPSWIdMSIuWUkkKfgLq_rvhddgTUCLr3qrO5gbx-8A5NGGrQqFW-HtUhZAg'}" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">${key.toUpperCase()}<br> (${key1.toLowerCase()})</h5>
+              <p class="card-text">${obj.Ram}<br>${obj.Rom}<br>${obj.Os}<br>${obj.camera}<br>${obj.color}</p>
+              <b>${obj.price}</b><br><br>
+              <div id="coloring" class"colors d-flex gap-3">
+              ${colorDiv}
+              </div><br>
+              <a href="#" class="btn btn-success">Add to Cart</a>
+            </div>
+          </div>
+    </div>
+        `
+        }
+    }
+}
+rendorAllPhones();
+function setModel() {
+    model.innerHTML = "";
+    model.innerHTML += `<option>Select Model</option>`;
+    // console.log("brand",brand.value)
+    for (var key in phones[brand.value]) {
+        model.innerHTML += `<option value="${key}">${key}</option>`;
+    }
+}
+var phoneDetail = document.getElementById('phone-detail');
+function searchCar() {
+    main.innerHTML = "";
+    main.style.display = "none";
+    phoneDetail.style.display = "flex";
+    var mobiles = phones[brand.value][model.value];
+    phoneDetail.innerHTML = `    
+    <div class="card mb-3">
+    <div class="row g-0">
+      <div class="col-md-4">
+        <img src="${mobiles.image || "https://lh3.googleusercontent.com/proxy/pvwN3dSU0Y-zxNZgURh2_Cfm5UYpl4WotrnDjNBti-oK6hhvPAMJYOnwVtfuD7guUa4yj9URbErWk3yZqz90EevAWpQ-yipuCZjRwsWkyXrMQOiQ4Bz8fgxv21ICHY09Lw"}" class="img-fluid rounded-start" alt="...">
+      </div>
+      <div class="col-md-8">
+        <div class="card-body">
+          <h5 class="card-title">${model.value.toUpperCase()}</h5>
+          <p class="card-text">${mobiles.Ram}<br>${mobiles.Rom}<br>${mobiles.Os}<br>${mobiles.camera}<br>${mobiles.color}</p>
+          <b>${mobiles.price}</b><br><br>
+        </div>
+      </div>
+    </div>
+  </div>
+    `;
+}
+function clearSreach() {
+    phoneDetail.innerHTML = "";
+    phoneDetail.style.display = "none";
+    main.style.display = "flex";
+    rendorAllPhones()
+}
